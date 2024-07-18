2d7b3343de04b514c0f093c2f6ae48e5d3e131bc
Himanshu <hjasaiwal114@gmail.com>
2024-07-18 20:40:13 +0530

    nav imp

--- my-project/src/components/Nav.jsx
+++ my-project/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
-import { navlink } from "../constants";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
@@ -15,25 +15,25 @@ const Nav = () => {
             className="m-0 w-[129px] h-[29px]"
           />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hdden">
-          {navlink.map((items) => (
-            <li key={items.label}>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
               <a
-                href={items.href}
+                href={item.href}
                 className="font-montserrat leading-normal text-lg text-slate-gray"
               >
-                {items.label}
+                {item.label}
               </a>
             </li>
           ))}
         </ul>
         <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
           <a href="/">Sign in</a>
-          <span></span>
+          <span>/</span>
           <a href="/">Explore now</a>
         </div>
         <div className="hidden max-lg:block">
-          <img src={hamburger} alt="humburger icon" width={25} height={25} />
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
         </div>
       </nav>
     </header>
